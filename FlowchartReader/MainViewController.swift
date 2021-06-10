e3f9da26f11c1ea563d93cd58f9756305900dda5
kristian <xtianlukito@gmail.com>
2021-06-10 19:26:55 +0700

    menu dgn view

--- FlowchartReader/MainViewController.swift
+++ FlowchartReader/MainViewController.swift
@@ -8,42 +8,59 @@
 import UIKit
 
 class MainViewController: UIViewController {
-    //daftar menu -> generate 3 menu
-    var menuList = MenuListStaticSeeder.getMenu()
-    
-    
-    @IBOutlet weak var tableMenu: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMenu()
         
-        tableMenu.dataSource = self
-        let nib = UINib(nibName: "MainMenuTableViewCell", bundle: nil)
-        tableMenu.register(nib, forCellReuseIdentifier: "menuCell")
-        
-//        tableMenu.rowHeight = tableMenu.contentSize.height / 4
     }
 
-//    override func viewWillAppear(_ animated: Bool) {
-//        tableMenu.rowHeight = tableMenu.contentSize.height / 4
-//    }
+    let cornerMenu = CGFloat(8)  //besar corner radius
+    
+    @IBOutlet weak var menu1Title1: UILabel!
+    @IBOutlet weak var menu1Title2: UILabel!
+    @IBOutlet weak var menu1Description: UILabel!
+    @IBOutlet weak var menu1Image: UIImageView!
+    @IBOutlet weak var menu1View: UIView!
+    
+    
+    @IBOutlet weak var menu2Title1: UILabel!
+    @IBOutlet weak var menu2Title2: UILabel!
+    @IBOutlet weak var menu2Description: UILabel!
+    @IBOutlet weak var menu2Image: UIImageView!
+    @IBOutlet weak var menu2View: UIView!
+    
+    @IBOutlet weak var menu3Title1: UILabel!
+    @IBOutlet weak var menu3Title2: UILabel!
+    @IBOutlet weak var menu3Description: UILabel!
+    @IBOutlet weak var menu3Image: UIImageView!
+    @IBOutlet weak var menu3View: UIView!
+    
+    
+    func setupMenu() {
+        let menuList = MenuListStaticSeeder.getMenu()
+        
+        menu1Title1.text = menuList[0].title1
+        menu1Title2.text = menuList[0].title2
+        menu1Description.text = menuList[0].description
+        menu1Image.image = menuList[0].getImage()
+        menu1View.backgroundColor = menuList[0].getColor()
+        menu1View.layer.cornerRadius = cornerMenu
+
+        menu2Title1.text = menuList[1].title1
+        menu2Title2.text = menuList[1].title2
+        menu2Description.text = menuList[1].description
+        menu2Image.image = menuList[1].getImage()
+        menu2View.backgroundColor = menuList[1].getColor()
+        menu2View.layer.cornerRadius = cornerMenu
+
+        menu3Title1.text = menuList[2].title1
+        menu3Title2.text = menuList[2].title2
+        menu3Description.text = menuList[2].description
+        menu3Image.image = menuList[2].getImage()
+        menu3View.backgroundColor = menuList[2].getColor()
+        menu3View.layer.cornerRadius = cornerMenu
+
+    }
 }
 
-
-extension MainViewController: UITableViewDataSource
-{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        menuList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell") as! MainMenuTableViewCell
-        
-        cell.menu = menuList[indexPath.row]
-        
-        return cell
-    }
-    
-    
-}

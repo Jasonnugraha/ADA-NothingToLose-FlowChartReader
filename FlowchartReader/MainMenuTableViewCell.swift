f4e4461f0cbfcc4e62f4b7d6ed121e3a96969231
kristian <xtianlukito@gmail.com>
2021-06-10 16:01:15 +0700

    mvc

--- FlowchartReader/MainMenuTableViewCell.swift
+++ FlowchartReader/MainMenuTableViewCell.swift
@@ -9,15 +9,27 @@ import UIKit
 
 class MainMenuTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var lbTitle1 : UILabel!
+    @IBOutlet weak var lbTitle2 : UILabel!
+    @IBOutlet weak var lbDescriptions : UILabel!
+    @IBOutlet weak var imMenu : UIImageView!
+    @IBOutlet weak var vwCell : UIView!
+    
+    var menu : Menu?
+    {
+        didSet{
+            setupView()
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func setupView() {
+        lbTitle1.text = menu?.title1
+        lbTitle2.text = menu?.title2
+        lbDescriptions.text = menu?.description
+        imMenu.image = menu?.getImage()
+        vwCell.layer.cornerRadius = 8
+        vwCell.backgroundColor = menu?.getColor()
     }
+    
     
 }
--- FlowchartReader/MainViewController.swift
+++ FlowchartReader/MainViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    //daftar menu -> generate 3 menu
+    var menuList = MenuListStaticSeeder.getMenu()
+    
+    
+    @IBOutlet weak var tableMenu: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableMenu.dataSource = self
+        let nib = UINib(nibName: "MainMenuTableViewCell", bundle: nil)
+        tableMenu.register(nib, forCellReuseIdentifier: "menuCell")
+        
+//        tableMenu.rowHeight = tableMenu.contentSize.height / 4
     }
 
-
+//    override func viewWillAppear(_ animated: Bool) {
+//        tableMenu.rowHeight = tableMenu.contentSize.height / 4
+//    }
 }
 
+
+extension MainViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        menuList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell") as! MainMenuTableViewCell
+        
+        cell.menu = menuList[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}

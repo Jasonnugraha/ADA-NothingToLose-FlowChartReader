fbec0ebd6a86993232bd0f5b68f330d05261d3f4
Jason Nugraha <jason.nugraha97@gmail.com>
2021-06-13 14:38:48 +0700

    Add new placeholder buat filesnya biar foldernya enggak ilang

---
+++ FlowchartReader/Features/Flowchart Reader/Model/PlaceholderHelper.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceholderHelper.swift
+//  FlowchartReader
+//
+//  Created by Jason Nugraha on 13/06/21.
+//
+
+import Foundation
---
+++ FlowchartReader/Themes/UI/PlaceholderUI.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceholderUI.swift
+//  FlowchartReader
+//
+//  Created by Jason Nugraha on 13/06/21.
+//
+
+import Foundation

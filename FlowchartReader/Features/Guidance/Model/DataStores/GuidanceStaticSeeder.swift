3552afbb0b8a712ab656abbba9ce423652360821
Jessi Febria <jfebria2302@gmail.com>
2021-06-17 20:03:54 +0700

    add logo, onboarding, launch screen

--- FlowchartReader/Features/Guidance/Model/DataStores/GuidanceStaticSeeder.swift
+++ FlowchartReader/Features/Guidance/Model/DataStores/GuidanceStaticSeeder.swift
@@ -13,12 +13,12 @@ class GuidanceStaticSeeder {
         var guidanceList = [Guidance]()
         
         guidanceList.append(Guidance(pGuidanceID: 0, pTitle: "What is flowchart?", pDescription: "A flowchart is simply a graphical representation of steps. It shows steps in sequential order and is widely used in presenting the flow of algorithms, workflow or processes. \nTypically, a flowchart shows the steps as boxes of various kinds, and their order by connecting them with arrows."))
-        guidanceList.append(Guidance(pGuidanceID: 1, pTitle: "What are the symbol in a flochart?",pDescription: "Flowcharts use special shapes to represent different types of actions or steps in a process. Lines and arrows show the sequence of the steps, and the relationships among them. These are known as flowchart symbols." +
+        guidanceList.append(Guidance(pGuidanceID: 1, pTitle: "What are the symbol in a flowchart?",pDescription: "Flowcharts use special shapes to represent different types of actions or steps in a process. Lines and arrows show the sequence of the steps, and the relationships among them. These are known as flowchart symbols." +
             "\nCommon Flowchart Symbols: " +
-            "\nRectangle Shape - Represents a process. " +
-            "\nOval Shape - Represents the start or end. " +
-            "\nDiamond Shape - Represents a decision. " +
-            "\nParallelogram - Represents input/output. "
+            "\nTerminator Symbol - Represents the start or end. " +
+            "\nProcess Symbol - Represents a process. " +
+            "\nDecision Symbol - Represents a decision. " +
+            "\nInput/Output Symbol - Represents an input/output. "
         ))
         guidanceList.append(Guidance(pGuidanceID: 2, pTitle: "How to read a flowchart?", pDescription: "Guidance: Read Flowchart"))
         guidanceList.append(Guidance(pGuidanceID: 3, pTitle: "How to save my flowchart", pDescription: "Guidance: Save my flowchart"))
@@ -27,3 +27,4 @@ class GuidanceStaticSeeder {
         return guidanceList
     }
 }
+
